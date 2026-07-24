5e8f58f28a4bfa79b633103014d9c7807d3f0428
nguyenduchung98 <nguyenduchung98@users.noreply.github.com>
2026-07-24 13:20:59 +0700

    style: add Lite design tokens and theme foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import './theme/tokens.css';
+import './theme/base.css';
 import './styles.css';
 import './translate.css';
 import './translate-extra.css';
